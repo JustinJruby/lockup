9df862363380bb182f20a4e5639e50a9b9684bdc
Justin <jevaniew@gmail.com>
2013-11-14 19:23:03 -0400

    Update lockup_controller.rb

--- app/controllers/lockup/lockup_controller.rb
+++ app/controllers/lockup/lockup_controller.rb
@@ -3,17 +3,19 @@ module Lockup
     skip_before_filter :check_for_lockup
     
     def unlock
-        puts params.inspect
+        puts 'params' + params.inspect
       if params[:lockup_codeword].present?
         user_agent = request.env['HTTP_USER_AGENT'].downcase
         unless user_agent.match(/crawl|googlebot|slurp|spider|bingbot|tracker|click|parser|spider/)
           @codeword = params[:lockup_codeword].to_s.downcase
           @return_to = params[:return_to]
           if @codeword == ENV["LOCKUP_CODEWORD"].to_s.downcase
+            puts 'Have Code word in GET'
             set_cookie
             run_redirect
           end
         else
+          puts 'Do Not have codeword'
           render :nothing => true
         end
       end
@@ -22,9 +24,11 @@ module Lockup
         @codeword = params[:lockup][:codeword].to_s.downcase
         @return_to = params[:lockup][:return_to]
         if @codeword == ENV["LOCKUP_CODEWORD"].to_s.downcase
+          puts 'codeword submit'
           set_cookie
           run_redirect
         else
+          puts 'bad codeword'
           @wrong = true
         end
       end
@@ -33,15 +37,17 @@ module Lockup
     private
     
     def set_cookie
-      puts cookies.inspect
       cookies[:lockup] = { :value => @codeword.to_s.downcase, :expires => (Time.now + 5.years) }
+            puts 'set cookie to  ' + cookies[:lockup].value 
     end
     
     def run_redirect
       if @return_to.present?
+        puts 'redirect to return to'
         redirect_to "#{@return_to}"
       else
-        redirect_to root_path
+        puts 'redirect to /'
+        redirect_to ::root_path
       end
     end
 
